2341ca10e0655e67d0531aad8ab8da2e4fdefa6a
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-05 02:46:04 -0700

    Added the blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Topic(models.Model):
@@ -8,3 +9,22 @@ class Topic(models.Model):
     def __str__(self):
         """String for representing the Model Object."""
         return self.name
+
+class BlogPost(models.Model):
+    """Model representing a blog post."""
+    title = models.CharField(max_length=200)
+    post_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=1000, help_text='Enter a description for you post')
+    topic = models.ManyToManyField(Topic, help_text='Select a topic for this blog post')
+
+    class Meta:
+        ordering = ['-post_date']
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this post."""
+        return reverse('blog-post-detail', args=[str(self.id)])
