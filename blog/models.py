df1b6f5bb26e869587757fe7a38f976fb960d616
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-05 01:49:47 -0700

    Added the Topic model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """Model representing a blog topic."""
+    name = models.CharField(max_length=200, help_text='Enter a blog topic (e.g Technology')
+
+    def __str__(self):
+        """String for representing the Model Object."""
+        return self.name
