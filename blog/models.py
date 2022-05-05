a9ec874f291795097eb1f4e7b0362880fdc41534
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-05 03:51:57 -0700

    Added the comment model

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class BlogPost(models.Model):
     
     def __str__(self):
         """String for representing the Model object."""
-        return self.title
+        return f'{self.title} - {self.author.user}'
     
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this post."""
@@ -43,3 +43,15 @@ class Author(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a deatil record for this user."""
         return reverse('author-detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    post_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=300, help_text='Enter comment about blog here')
+
+    class Meta:
+        ordering=['post_date']
+    
+    def __str__(self):
+        """String for representing Comment object."""
+        return f'{self.post_date} - {self.author.user} - {self.description}'
