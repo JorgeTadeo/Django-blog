c4e00fcf2f4a67f23bbb85d8b284c3ce0a3ca6e6
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-05 23:34:09 -0700

    Added some comments explaining my model fields and added a foreign key on comments refering to a blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.urls import reverse
 
@@ -14,11 +15,15 @@ class BlogPost(models.Model):
     """Model representing a blog post."""
     title = models.CharField(max_length=200)
     post_date = models.DateField(auto_now_add=True)
+    # A blog post may only have one author but many authors can have many blog posts
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    # A text field that holds the contents of our blog post
     description = models.TextField(max_length=1000, help_text='Enter a description for you post')
+    # A Blog Post can have many topics and a topic can be applied to many blog posts
     topic = models.ManyToManyField(Topic, help_text='Select a topic for this blog post')
 
     class Meta:
+        # order the blog posts in reverse cronological order
         ordering = ['-post_date']
     
     def __str__(self):
@@ -45,11 +50,16 @@ class Author(models.Model):
         return reverse('author-detail', args=[str(self.id)])
 
 class Comment(models.Model):
+    # Every comment must have an Author and Authors can have many comments
     author = models.ForeignKey(Author, on_delete=models.SET_NULL)
     post_date = models.DateTimeField(auto_now_add=True)
+    # A text field of what comment is 
     description = models.TextField(max_length=300, help_text='Enter comment about blog here')
+    # A blog post the comment refers too
+    blogpost =models.ForeignKey(BlogPost, on_delete=models.CASCADE)
 
     class Meta:
+        # order the comments in cronological order according to post date
         ordering=['post_date']
     
     def __str__(self):
