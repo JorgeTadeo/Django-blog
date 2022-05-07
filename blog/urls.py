facd4ed3efa361c9e36333ebc724223358af5126
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-07 03:02:09 -0700

    Added a url mapping, generic view and template to list all blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('blogposts/', views.BlogPostView.as_view(), name='blogposts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Topic, BlogPost, Author, Comment
+import django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -17,3 +18,6 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BlogPostView(generic.ListView):
+    model = BlogPost
