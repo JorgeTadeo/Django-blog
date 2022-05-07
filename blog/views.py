27aef15af8216eb5b446db7a85c0729f41b0e12d
jorgetadeo <jorge_tadeo@icloud.com>
2022-05-07 01:51:22 -0700

    added index url mapping and its view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Topic, BlogPost, Author, Comment
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_blogposts = BlogPost.objects.count()
+    num_authors = Author.objects.count()
+    num_comments = Comment.objects.count()
+    
+    context = {
+        'num_blogposts:': num_blogposts,
+        'num_authors': num_authors,
+        'num_comments': num_comments,
+    }
+
+    return render(request, 'index.html', context=context)
